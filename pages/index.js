016180146f2639688f8bcd0ea110427715340de5
Md Shoaib Mehedi <mdshoaibmehedi@Mds-MacBook-Air.local>
2021-11-28 14:39:48 +0600

    next auth

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Layout from '../components/layout'
+
+export default function Page () {
+  return (
+    <Layout>
+      <h1>Landing page</h1>
+      <p>
+        Landing Description
+      </p>
+    </Layout>
+  )
+}
